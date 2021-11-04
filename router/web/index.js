2ccd5ac8b7f7bb33b466841a584a3ebfc12b7d5f
Nattinee Unarinae <73813936+Fonochy@users.noreply.github.com>
2021-11-04 22:13:46 +0700

    third

--- router/web/index.js
+++ router/web/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-router.get("/", (req, res) => {
+router.get("/index", (req, res) => {
     res.sendFile(path.join(__dirname+'/frontend/index.html'))
 })
 
