2d4a22b1ebbc5e5c3e71f540fae573c99431f9ba
cherrystz <plug4869@gmail.com>
2021-11-08 22:47:45 +0700

    แก้ครั้งที่ 1

--- router/api/users.js
+++ router/api/users.js
@@ -4,28 +4,24 @@ var router = express.Router();
 var UsersModel = require("../../schema/user");
 var Response = require("../../response");
 
-router.get("/", function (req, res, next) {
-    res.render("index");
-  });
+/* POST Data. */
+router.post("/add-users-api", function (req, res, next) {
+  console.log(req.body);
 
-  /* POST Data. */
-  router.post("/add-users-api", function (req, res, next) {
-    console.log(req.body);
-  
-    const mybodydata = {
-      user_firstname: req.body.user_firstname,
-      user_email: req.body.user_email,
-      user_mobile: req.body.user_mobile,
-    };
-    var data = UsersModel(mybodydata);
-    //var data = UsersModel(req.body);
-    data.save(function (err) {
-      if (err) {
-        Response.errorResponse(err, res);
-      } else {
-        Response.successResponse("User Added!", res, {});
-      }
-    });
+  const mybodydata = {
+    user_firstname: req.body.user_firstname,
+    user_email: req.body.user_email,
+    user_mobile: req.body.user_mobile,
+  };
+  var data = UsersModel(mybodydata);
+  //var data = UsersModel(req.body);
+  data.save(function (err) {
+    if (err) {
+      Response.errorResponse(err, res);
+    } else {
+      Response.successResponse("User Added!", res, {});
+    }
   });
+});
 
 module.exports = router;
