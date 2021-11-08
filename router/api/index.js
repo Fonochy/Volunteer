79ef189b8fc5c95b198ea766681244c6304c48c2
MinnoZappp <73789124+MinnoZappp@users.noreply.github.com>
2021-11-08 21:58:40 +0700

    updateล่าสุด

--- router/api/index.js
+++ router/api/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("test")
-})
+router.get("/", function (req, res, next) {
+    res.render("index");
+  });
 
 module.exports = router;
